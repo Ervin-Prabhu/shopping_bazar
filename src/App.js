726f03cc7df1600331a51ad8ad32f79fb8d5481c
Ervin Prabhu <ervinprabhu5@gmail.com>
2024-07-11 03:31:17 +0530

    main routing established

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
+import { Route, Routes } from "react-router-dom";
+import Home from "./components/Home/Home";
+import Products from "./components/Products/Products";
+import Cart from "./components/Cart/Cart";
 
 function App() {
   return (
-    <div >
-    </div>
+    <Routes>
+      <Route path="/" element={<Home />}/>
+      <Route path="/products" element={<Products />}/>
+      <Route path="/cart" element={<Cart />}/>
+    </Routes>
   );
 }
 
